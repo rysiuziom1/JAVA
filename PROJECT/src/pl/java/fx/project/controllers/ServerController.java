2b5f647b89542e3af5bc7aaccb65aa5bbfbca939
Jacek <rysiuziom1@gmail.com>
2019-06-16 15:29:15 +0200

    START FILE RECEIVER

--- PROJECT/src/pl/java/fx/project/controllers/ServerController.java
+++ PROJECT/src/pl/java/fx/project/controllers/ServerController.java
@@ -1,5 +1,6 @@
 package pl.java.fx.project.controllers;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TreeView;
@@ -7,7 +8,9 @@ import javafx.scene.layout.StackPane;
 import pl.java.fx.project.treebuilder.FileTreeBuilder;
 import pl.java.fx.project.treebuilder.RefreshFileTree;
 
-import java.io.File;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executors;
@@ -37,8 +40,44 @@ public class ServerController {
 
     public void init() {
         RefreshFileTree refreshTask = new RefreshFileTree(serverTreeView, allFilesList);
-        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(2);
         scheduler.scheduleWithFixedDelay(refreshTask, 0, 1, TimeUnit.SECONDS);
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(5056);
+        } catch (IOException e) {
+            e.printStackTrace();
+            Platform.exit();
+        }
+
+        ServerSocket finalServerSocket = serverSocket;
+        Runnable acceptClientTask = () -> Platform.runLater(() -> {
+            Socket socket = null;
+            ObjectInputStream inputStream = null;
+            ObjectOutputStream outputStream = null;
+
+            try {
+                socket = finalServerSocket.accept();
+                inputStream = new ObjectInputStream(socket.getInputStream());
+                outputStream = new ObjectOutputStream(socket.getOutputStream());
+                ObjectInputStream finalInputStream = inputStream;
+                Runnable listenClientTask = () -> Platform.runLater(() -> {
+                    try {
+                        String received = (String) finalInputStream.readObject();
+                        if(received.equalsIgnoreCase("Send file")) {
+                            
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
 
         mainStackPane.getScene().getWindow().setOnCloseRequest(e ->{
             scheduler.shutdown();
